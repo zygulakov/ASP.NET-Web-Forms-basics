e04653f92801564654aaaabc6ee903100365a644
zygulakov <azizgulakov1997@gmail.com>
2020-11-03 08:20:25 +0700

    added Cookies

--- WebFormsDemo/Contact.aspx.cs
+++ WebFormsDemo/Contact.aspx.cs
@@ -7,11 +7,22 @@ using System.Web.UI.WebControls;
 
 namespace WebFormsDemo
 {
-    public partial class Contact : System.Web.UI.Page
+    public partial class Contact : Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Request.Cookies["userContactInfo"] != null)
+            {
+                HttpCookie userContactInfo = Request.Cookies["userContactInfo"];
+                firstNameBox.Text = userContactInfo["firstName"];
+                lastNameBox.Text = userContactInfo["lastName"];
+                birthDayTextBox.Text = userContactInfo["birthDay"];
+                emailBox.Text = userContactInfo["email"];
+                genderCheckBoxList.SelectedValue = userContactInfo["gender"];
+                registeredCheckBox.Checked = userContactInfo["isRegistered"].Equals("registered")? true:false;
+                countryDropDownList.SelectedValue = userContactInfo["country"];
 
+            }
         }
 
         protected void sendEmailButton_Click(object sender, EventArgs e)
@@ -31,6 +42,16 @@ namespace WebFormsDemo
             resultGender.Text = gender;
             resultRegistered.Text = isRegistered;
             resultCountry.Text = country;
+            //saving data  as a cookies
+            HttpCookie userContactInfo = new HttpCookie("userContactInfo");
+            userContactInfo["firstName"] = firstName;
+            userContactInfo["lastName"] = lastName;
+            userContactInfo["birthDay"] = birthDay;
+            userContactInfo["email"] = email;
+            userContactInfo["gender"] = gender;
+            userContactInfo["isRegistered"] = isRegistered;
+            userContactInfo["country"] = country;
+            Response.Cookies.Add(userContactInfo);
         }
     }
 }
